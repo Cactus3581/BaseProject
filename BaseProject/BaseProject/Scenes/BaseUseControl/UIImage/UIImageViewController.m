f5d759b54e17f56405e3c722214c15298a9b9c05
xiaruzhen <tytyty3581@163.com>
2018-01-21 23:44:03 +0800

    layer

---
+++ BaseProject/BaseProject/Scenes/BaseUseControl/UIImage/UIImageViewController.m
@@ -0,0 +1,25 @@
+//
+//  UIImageViewController.m
+//  BaseProject
+//
+//  Created by xiaruzhen on 2018/1/21.
+//  Copyright © 2018年 cactus. All rights reserved.
+//
+
+#import "UIImageViewController.h"
+
+@interface UIImageViewController ()
+
+@end
+
+@implementation UIImageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
